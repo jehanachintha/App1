36031a48c3419a49ca17cf7efe5aaac99d9a88b5
jehanachintha <91360323+jehanachintha@users.noreply.github.com>
2021-09-25 00:49:10 -0400

    Add files via upload

---
+++ App1/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace App1.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public double Temperature { get; set; }
+        public double Flowrate { get; set; } 
+        public void Setvalues(int Temperature, int Flowrate)
+        {
+                Id = Guid.NewGuid().ToString();
+                this.Temperature = Temperature;
+                this.Flowrate = Flowrate;
+        }
+    }
+}
